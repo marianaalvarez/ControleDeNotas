9f587bbe16a1295077b0a2552380faac82f7238b
Guilherme Bayma <gfbayma@gmail.com>
2015-06-23 17:44:30 -0300

    View de editar funcionando.

--- SemNome/SemNome/EditarViewController.swift
+++ SemNome/SemNome/EditarViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class EditarViewController: UIViewController, UITextFieldDelegate {
+class EditarViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
     var atividade: Atividade?
+    var data : NSDate?
+    let tiposDeAvaliacoes = ["Prova","Trabalho"]
     
     @IBOutlet weak var disciplinaTextField: UITextField!
     @IBOutlet weak var dataLabel: UILabel!
     @IBOutlet weak var tipoLabel: UILabel!
     @IBOutlet weak var notaTextField: UITextField!
-    var data : NSDate?
+    @IBOutlet weak var selecionarBotao: UIButton!
+
+    @IBOutlet weak var datePickerSubView: UIView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var pickerViewSubView: UIView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        selecionarBotao.alpha = 0
+        
+        datePickerSubView.hidden = true
+        pickerViewSubView.hidden = true
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
         
         disciplinaTextField.delegate = self
         notaTextField.delegate = self
@@ -51,7 +67,52 @@ class EditarViewController: UIViewController, UITextFieldDelegate {
         notaTextField.resignFirstResponder()
         return true
     }
-
+    
+    @IBAction func alterarData(sender: UIButton) {
+        selecionarBotao.alpha = 1
+        datePickerSubView.hidden = false
+        pickerViewSubView.hidden = true
+    }
+    
+    @IBAction func alterarTipo(sender: UIButton) {
+        selecionarBotao.alpha = 1
+        pickerViewSubView.hidden = false
+        datePickerSubView.hidden = true
+        tipoLabel.text = "Prova"
+    }
+    
+    // PickerView DataSource
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tiposDeAvaliacoes.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return tiposDeAvaliacoes[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        tipoLabel.text = tiposDeAvaliacoes[row]
+    }
+    
+    // DatePicker action
+    @IBAction func mudarValorDatePicker(sender: UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+        dataLabel.text = dateFormatter.stringFromDate(datePicker.date)
+        data = datePicker.date
+    }
+    
+    @IBAction func acaoSelecionar(sender: UIButton) {
+        selecionarBotao.alpha = 0
+        pickerViewSubView.hidden = true
+        datePickerSubView.hidden = true
+    }
+    
     @IBAction func salvar(sender: UIBarButtonItem) {
         if !disciplinaTextField.text.isEmpty {
             atividade?.nome = disciplinaTextField.text
