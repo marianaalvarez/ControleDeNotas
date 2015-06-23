a86cec40d9b57826275206934a98dbe131b0fb6d
Mariana Alvarez <marianaalvarezdecarvalho@hotmail.com>
2015-06-23 17:21:34 -0300

    Alterações no método de criar notificação.

--- SemNome/SemNome/LocalNotificationManager.swift
+++ SemNome/SemNome/LocalNotificationManager.swift
@@ -16,28 +16,21 @@ class LocalNotificationManager {
     
     func criaNotificacao(atividade: Atividade) {
         
-        for i in 0...7 {
-            var calendar = NSCalendar.currentCalendar()
-            var components = calendar.components(NSCalendarUnit.YearCalendarUnit|NSCalendarUnit.MonthCalendarUnit|NSCalendarUnit.DayCalendarUnit|NSCalendarUnit.HourCalendarUnit|NSCalendarUnit.MinuteCalendarUnit, fromDate: atividade.dia)
-            components.day -= i
+        var calendar = NSCalendar.currentCalendar()
+        var components = calendar.components(.CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay | .CalendarUnitHour | .CalendarUnitMinute, fromDate: atividade.dia)
+        
+        components.day = components.day - 1
             
-            var novaData = calendar.dateFromComponents(components)
+        var novaData = calendar.dateFromComponents(components)
             
-            var notification = UILocalNotification()
-            
-            if i == 0 {
-                notification.alertBody = "A entrega da atividade \(atividade.nome) da disciplina \(atividade.disciplina.nome) é hoje!"
-            } else if i == 1 {
-                notification.alertBody = "A entrega da atividade \(atividade.nome) da disciplina \(atividade.disciplina.nome) é amanhã!"
-            } else {
-                notification.alertBody = "Faltam \(i) dias para a entrega da atividade \(atividade.nome) da disciplina \(atividade.disciplina.nome)."
-            }
-            notification.fireDate = novaData
-            notification.soundName = UILocalNotificationDefaultSoundName
-            notification.userInfo = ["disciplina": atividade.disciplina.nome, "atividade": atividade.nome]
-            notification.category = "ANOTAI_CATEGORY"
-            UIApplication.sharedApplication().scheduleLocalNotification(notification)
-            println(notification.userInfo!["atividade"]!)
-        }
+        var notification = UILocalNotification()
+        notification.alertBody = "A entrega da atividade \(atividade.nome) da disciplina \(atividade.disciplina.nome) é amanhã!"
+        notification.fireDate = novaData
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.userInfo = ["disciplina": atividade.disciplina.nome, "atividade": atividade.nome]
+        notification.category = "ANOTAI_CATEGORY"
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        println(notification.userInfo!["atividade"]!)
+        
     }
 }
